94f3157b2190a29571dc2454504475899def401b
RaLaX90 <cool.suhomlin@mail.ru>
2020-05-14 16:50:20 +0300

    Add new checks, the names of variables and the structure of the program are slightly changed (textbox added).

---
+++ WindowsFormsApp1/CustomViewLinkParser.cs
@@ -0,0 +1,83 @@
+﻿using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class CustomViewLinkParser
+    {
+        public CustomViewLinkParser(string rootLink = "http://localhost:50897")
+        {
+            this.rootLink = rootLink;
+
+            exceptions = File.ReadAllLines("text.txt");
+
+            exceptionsCount = exceptions.Length;
+        }
+        
+        private string rootLink;
+
+        private string[] exceptions;
+
+        private int exceptionsCount;
+
+        private void WriteToFile(string link)
+        {
+            using (FileStream fstream = new FileStream("text2.txt", FileMode.Append, FileAccess.Write, FileShare.None))
+            using (var write = new StreamWriter(fstream))
+            {
+                write.WriteLine(link);
+            }
+        }
+
+        public void Parse1(string startParseLink)
+        {
+            var web = new HtmlWeb();
+            var currentLink = startParseLink;
+            HtmlAgilityPack.HtmlDocument doc = web.Load(rootLink + currentLink);
+
+            var nodes = doc.DocumentNode.CssSelect("a").ToList();
+
+            int counter = 0;
+            string link;
+
+            WriteToFile("\r\n!!!" + rootLink + currentLink + "!!!");
+            foreach (var node in nodes)
+            {
+                //Console.WriteLine(node.InnerHtml);
+
+                link = node.GetAttributeValue("href");
+
+                if ((link.Length > 0) && link[0] != '#' && (link.IndexOf("datamaskingwiki") < 0) && (link.IndexOf("mailto") < 0) 
+                    && (link.IndexOf("http") < 0) && (link.IndexOf("https") < 0) && (link.IndexOf("tel") < 0))
+                {
+                    counter = 0;
+
+                    foreach (string tempLink in exceptions)
+                    {
+                        if (tempLink == link)
+                        {
+                            break;
+                        }
+                        counter++;
+                    }
+
+                    if (counter == exceptionsCount)
+                    {
+                        WriteToFile(link);
+                        //Console.WriteLine("Unique link, which was written to the file, = " + link);
+                    }
+                }
+
+            }
+
+            MessageBox.Show("Parse is finished");
+        }
+    }
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -26,20 +26,6 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        public void WriteToFile(string link)
-        {
-            using (FileStream fstream = new FileStream("text2.txt", FileMode.Open))
-            {
-                // преобразуем строку в байты
-                link = "\r\n" + link;
-                byte[] array = System.Text.Encoding.Default.GetBytes(link);
-                // запись массива байтов в файл
-                fstream.Seek(fstream.Length, SeekOrigin.Begin);
-                fstream.Write(array, 0, array.Length);
-                //Console.WriteLine("Текст записан в файл");
-            }
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             //var txtHTML = GetPage(@"https://joblab.ru/search.php?r=vac&srregion=100&maxThread=100&submit=1");
@@ -47,72 +33,10 @@ namespace WindowsFormsApp1
             //doc.LoadHtml(txtHTML);
             //label1.Text = doc.ParsedText.ToString();
 
-            //using (FileStream fstream = new FileStream("text.txt", FileMode.Open))
-            //{
-            //    // преобразуем строку в байты
-            //    byte[] array = new byte[fstream.Length];
-            //    // считываем данные
-            //    fstream.Read(array, 0, array.Length);
-            //    //fstream.Seek(array.Length / 2, SeekOrigin.Current);
-            //    // декодируем байты в строку
-            //    textFromFile[0] = System.Text.Encoding.Default.GetString(array);
-            //    Console.WriteLine($"Текст из файла: {textFromFile}");
-            //}
+            var Parser = new CustomViewLinkParser();
 
-            List<string> textFromFile = new List<string>();
+            Parser.Parse1(textBox1.Text);
 
-            string link = "";
-            {
-
-
-                // Read the file and display it line by line.  
-                System.IO.StreamReader file = new System.IO.StreamReader("text.txt");
-                while ((link = file.ReadLine()) != null)
-                {
-                    textFromFile.Add(link);
-                }
-
-                textFromFile.Capacity = textFromFile.Count;
-                file.Close();
-                link = "";
-            }
-
-            var web = new HtmlWeb();
-            var rootLink = "http://localhost:50897/Products.aspx";
-            HtmlDocument doc = web.Load(rootLink);
-
-            var nodes = doc.DocumentNode.CssSelect("a").ToList();
-            int counter = 0;
-
-            foreach (var node in nodes)
-            {
-                //Console.WriteLine(node.InnerHtml);
-
-                link = node.GetAttributeValue("href");
-
-                if((link.Length > 0) && link[0] != '#')
-                {
-                    counter = 0;
-
-                    foreach (string tempLink in textFromFile)
-                    {
-                        if (tempLink == link)
-                        {
-                            break;
-                        }
-                        counter++;
-                    }
-
-                    if (counter == 51)
-                    {
-                        WriteToFile(link);
-                        //Console.WriteLine("Unique link, which was written to the file, = " + link);
-                    }
-                }
-                
-            }
-
-            MessageBox.Show("Parse is finished");
         }
     }
 }
